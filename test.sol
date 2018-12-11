d371e451265ee067b64049a006da588f5dc68353
uhmhaha <uhmhaha@nate.com>
2018-12-11 20:59:37 +0900

    Update test.sol

--- test.sol
+++ test.sol
@@ -11,34 +11,42 @@ contract EventTickets {
     
     struct Buyer {
         string buyerName;
-        uint ticketQuantity;
+        uint buyingQauntity;
     }
     
     struct Ticket {
         uint costPerTicket;
         string description;
         string webSite;
+        uint ticketQuantity;
         mapping(string => Buyer) buyers;
         string[] buyersAccts;
+        //Buyer[] buyers;
 
     }
     
     mapping(address => Ticket) tickets;
     address[] public eventAccts;
 
-    function createEvent( uint cost, string des, string web, string nam, uint qua) public payable{
+    function createEvent( uint cost, string des, string web, uint qua) public payable{
         
         var ticket = tickets[msg.sender];
-        var buyer = ticket.buyers[nam];
+        //var buyer = ticket.buyers[nam];
         
         ticket.costPerTicket = cost;
         ticket.description = des;
         ticket.webSite = web;
+        ticket.ticketQuantity = qua;
+        // ticket.buyers.push(Buyer ({
+        //     buyerName = nam,
+        //     ticketQuantity = qua
+        // }));
+       // buyer.buyerName = nam;
+        //buyer.ticketQuantity = qua;
         
-        buyer.buyerName = nam;
-        buyer.ticketQuantity = qua;
-        
-        ticket.buyersAccts.push(nam) -1;
+        //ticket.buyersAccts.push(nam) -1;
+        //ticket.buyers[nam].buyerName = nam;
+        //ticket.buyers[nam].ticketQuantity = qua;
         eventAccts.push(msg.sender) -1;
     }
     
@@ -46,20 +54,23 @@ contract EventTickets {
          return eventAccts;
     }
     
-    function getEvent(string name) view public returns(uint, string, string, string, uint){
-      return (tickets[msg.sender].costPerTicket, tickets[msg.sender].description, tickets[msg.sender].webSite, tickets[msg.sender].buyers[name].buyerName, tickets[msg.sender].buyers[name].ticketQuantity);
+    function getEvents() view public returns(uint, string, string, uint){
+      return (tickets[msg.sender].costPerTicket, tickets[msg.sender].description, tickets[msg.sender].webSite, tickets[msg.sender].ticketQuantity);
     }
     
     function buyTicket( string name, uint q) public payable {
-        require(tickets[msg.sender].buyers[name].ticketQuantity > q);
-        thisIsBalance += tickets[msg.sender].buyers[name].ticketQuantity * q;
-        tickets[msg.sender].buyers[name].ticketQuantity -= q;
+        require(tickets[msg.sender].ticketQuantity > q);
+        thisIsBalance += tickets[msg.sender].ticketQuantity * q;
+        tickets[msg.sender].ticketQuantity -= q;
+        var buyer = tickets[msg.sender].buyers[name];
+        buyer.buyingQauntity = q;
+        buyer.buyerName = name;
     }
 
     function refund(string name,uint q) public payable {
-        require(thisIsBalance >= tickets[msg.sender].buyers[name].ticketQuantity * q);
-        thisIsBalance += tickets[msg.sender].buyers[name].ticketQuantity * q;
-        tickets[msg.sender].buyers[name].ticketQuantity += q;
+        require(thisIsBalance >= tickets[msg.sender].buyers[name].buyingQauntity * q);
+        thisIsBalance += tickets[msg.sender].buyers[name].buyingQauntity * q;
+        tickets[msg.sender].ticketQuantity += q;
         
         if(msg.sender.call.value(shares[msg.sender])())
             shares[msg.sender] = 0;
